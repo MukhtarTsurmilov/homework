679205442fd6934c635da8f6e0605afc7ac53e10
Mukhtar <mr.moshka@gmail.com>
2020-05-30 19:18:26 +0300

    Практика ч3.
    Добавлены функции

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели ?"),
+let numberOfFilms,
     personalMovieDB = {
         conut: numberOfFilms,
         movies: {},
@@ -9,18 +9,32 @@ let numberOfFilms = prompt("Сколько фильмов вы уже посмо
         privat: false
     };
 
+
+//Функция start - запускается первый вопрос о количестве фильмов
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ?", ''); //сам вопрос
+    
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) { //цикл проверки, если строка пустая или нажата "отмена" вопрос задается еще раз
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ?", '');
+    }
+}
+start(); //запуск функции 
+
 // Цикл методом for
-// for (let i = 0; i < 2; i++) { //Цикл с вопросами
-//     let lastMovie = prompt("Один из последних фильмов?", ""),
-//         raiting = prompt("На сколько оцените его?", "");
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) { //Цикл с вопросами
+        let lastMovie = prompt("Один из последних фильмов?", ""),
+            raiting = prompt("На сколько оцените его?", "");
 
-//     if (lastMovie == "" || raiting == "" || lastMovie == null || raiting == null || lastMovie.length > 50) { //Запреть вводить пустую строку, название фильма длинне 50 символов и отменять ввод
-//         i--;
-//     } else {
-//         personalMovieDB.movies[lastMovie] = raiting; //Если все нормально то записать ответы и поместить в объект
-//     }
+        if (lastMovie == "" || raiting == "" || lastMovie == null || raiting == null || lastMovie.length > 50) { //Запреть вводить пустую строку, название фильма длинне 50 символов и отменять ввод
+            i--;
+        } else {
+            personalMovieDB.movies[lastMovie] = raiting; //Если все нормально то записать ответы и поместить в объект
+        }
 
-// }
+    }
+}
+rememberMyFilms(); //запуск функции 
 
 // Цикл методом while
 // let i = 0;
@@ -37,32 +51,53 @@ let numberOfFilms = prompt("Сколько фильмов вы уже посмо
 // }
 
 // Цикл методом do...while
-let i = 0;
- do {
-    let lastMovie = prompt("Один из последних фильмов?", ""),
-        raiting = prompt("На сколько оцените его?", "");
+// let i = 0;
+//  do {
+//     let lastMovie = prompt("Один из последних фильмов?", ""),
+//         raiting = prompt("На сколько оцените его?", "");
 
-    if (lastMovie == "" || raiting == "" || lastMovie == null || raiting == null || lastMovie.length > 50) { //Запреть вводить пустую строку, название фильма длинне 50 символов и отменять ввод
-        i--;
-        console.log(i);
+//     if (lastMovie == "" || raiting == "" || lastMovie == null || raiting == null || lastMovie.length > 50) { //Запреть вводить пустую строку, название фильма длинне 50 символов и отменять ввод
+//         i--;
+//         console.log(i);
+//     } else {
+//         personalMovieDB.movies[lastMovie] = raiting;  //Если все нормально то записать ответы и поместить в объект
+//         i++;
+//         console.log(i);
+
+//     }
+// }
+
+// while (i < 2);
+
+function detectedPersonalLevel() { //фунция writeYourGenres
+    if (personalMovieDB.conut < 10) { //Вывовд сообщения в зависимости от количества просмотренных фильмов
+        alert("Просмотроено довольно мало фильмов");
+    } else if (10 <= personalMovieDB.conut && personalMovieDB.conut < 30) {
+        alert("Вы классный зритель");
+    } else if (personalMovieDB.conut >= 30) {
+        alert("Вы киноман");
     } else {
-        personalMovieDB.movies[lastMovie] = raiting;  //Если все нормально то записать ответы и поместить в объект
-        i++;
-        console.log(i);
-
+        alert("Произошла ошибка");
     }
 }
+detectedPersonalLevel(); //запуск функции detectedPersonalLevel
 
-while (i < 2);
+function writeYourGenres() { //фунция writeYourGenres
+    for (let i = 0; i < 3; i++) { //цикл выполняется три раза
+        let favoriteGenres = prompt(`Ваш люимый жанр под номером ${i+1} ?`, ""); //вопрос с интерполяцией
 
-if (personalMovieDB.conut < 10) {                   //Вывовд сообщения в зависимости от количества просмотренных фильмов
-    alert("Просмотроено довольно мало фильмов");
-} else if (10 <= personalMovieDB.conut && personalMovieDB.conut < 30) {
-    alert("Вы классный зритель");
-} else if (personalMovieDB.conut >= 30) {
-    alert("Вы киноман");
-} else {
-    alert("Произошла ошибка");
+        if (favoriteGenres == "" || favoriteGenres == null) {  //цикл проверки, если строка пустая или нажата "отмена" вопрос задается еще раз
+            i--;
+        } else {
+            personalMovieDB.genres[i] = favoriteGenres;
+        }
+    }
 }
+writeYourGenres(); //запуск функции writeYourGenres
 
-console.log(personalMovieDB);
+function showMyDB() { //фунция showMyDB 
+    if (personalMovieDB.privat == false) { //проверка условия, если если знаение ключа "private" соотвествует "false" тогда в консоль выводится информация по объекту
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(); //запуск функции showMyDB
